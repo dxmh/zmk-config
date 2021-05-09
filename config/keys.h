bd1bbabee0c88d3501faed1fa915658c1d76e40f
Dom H <dom@hxy.io>
2021-05-09 22:35:42 +0100

    Move custom key codes to separate file
    I plan to add a few more to this list (for macOS, etc.)

---
+++ config/keys.h
@@ -0,0 +1,12 @@
+/*
+ * For the full list of ZMK keycodes refer to
+ * zmk/app/include/dt-bindings/zmk/keys.h
+ */
+
+/* UK layout */
+#define UK_DOUBLE_QUOTES (AT_SIGN)
+#define UK_AT_SIGN (DOUBLE_QUOTES)
+#define UK_PIPE (PIPE2)
+#define UK_TILDE (TILDE2)
+#define UK_HASH (NON_US_HASH)
+#define UK_BACKSLASH (NON_US_BACKSLASH)
