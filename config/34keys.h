1f7c3ab17ed487fe220bdb141df466a6559a1ec6
Dom H <dom@hxy.io>
2021-08-01 10:44:58 +0100

    Move key aliases into header file

---
+++ config/34keys.h
@@ -0,0 +1,25 @@
+#define XXX &none
+#define ___ &trans
+
+#define BASE 0
+#define NAV 1
+#define SYM 2
+#define ADJ 3
+#define BASE_IOS 4
+#define NAV_IOS 5
+
+#define NEXT LC(PG_DN)
+#define PREV LC(PG_UP)
+#define SW_APP LG(TAB)
+#define WORD_BS LC(BSPC)
+#define WORD_L LC(LEFT)
+#define WORD_R LC(RIGHT)
+
+#define iEND LG(RIGHT)
+#define iHOME LG(LEFT)
+#define iNEXT LS(LG(RBKT))
+#define iPREV LS(LG(LBKT))
+#define iWORD_BS LA(BSPC)
+#define iWORD_L LA(LEFT)
+#define iWORD_R LA(RIGHT)
+
