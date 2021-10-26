3fa76b8d8d05091c9f008c976531734c474c8d35
Dom H <dom@hxy.io>
2021-10-26 20:44:09 +0100

    Remove unused key definitions

--- config/34keys.h
+++ config/34keys.h
@@ -6,21 +6,6 @@
 #define NUM 2
 #define BASE_IOS 3
 
-#define NEXT LC(PG_DN)
-#define PREV LC(PG_UP)
-#define SW_APP LG(TAB)
-#define WORD_BS LC(BSPC)
-#define WORD_L LC(LEFT)
-#define WORD_R LC(RIGHT)
-
-#define iEND LG(RIGHT)
-#define iHOME LG(LEFT)
-#define iNEXT LC(TAB)
-#define iPREV LS(LC(TAB))
-#define iWORD_BS LA(BSPC)
-#define iWORD_L LA(LEFT)
-#define iWORD_R LA(RIGHT)
-
 // Apple's "Globe key" is mapped to Caps Lock at the operating system level
 // https://github.com/zmkfirmware/zmk/issues/947
 #define GLOBE CAPSLOCK
