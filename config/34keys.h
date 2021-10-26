322a24b7c4c7ca44ddddcff4aa41339ca560928d
Dom H <dom@hxy.io>
2021-10-26 20:53:22 +0100

    Remove separate iPadOS layer
    Things are simpler this way.

--- config/34keys.h
+++ config/34keys.h
@@ -4,7 +4,6 @@
 #define BASE 0
 #define NAV 1
 #define NUM 2
-#define BASE_IOS 3
 
 // Apple's "Globe key" is mapped to Caps Lock at the operating system level
 // https://github.com/zmkfirmware/zmk/issues/947
