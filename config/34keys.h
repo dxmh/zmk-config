2d5ad8fe24c0912724fb37082d5665ba1bea4eae
Dom H <dom@hxy.io>
2021-11-09 09:29:07 +0000

    Remove mod-taps (again)...

--- config/34keys.h
+++ config/34keys.h
@@ -8,7 +8,3 @@
 // Apple's "Globe key" is mapped to Caps Lock at the operating system level
 // https://github.com/zmkfirmware/zmk/issues/947
 #define GLOBE CAPSLOCK
-
-// Key positions
-#define KEYPOS_LEFT_HALF <0 1 2 3 4 10 11 12 13 14 20 21 22 23 24>
-#define KEYPOS_RIGHT_HALF <5 6 7 8 9 15 16 17 18 19 25 26 27 28 29>
