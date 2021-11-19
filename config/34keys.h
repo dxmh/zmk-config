b418b657aef908b88645d874d8e95a37604c1f69
Dom H <dom@hxy.io>
2021-11-19 22:44:27 +0000

    Use a different nav layer for Linux and Mac hosts
    On macOS, GUI + arrow keys are used to achieve page up, page down, home
    and end. For a consistent experience on Linux, this behaviour is
    achieved through the use of mod morphs.
    
    Switching between hosts is now done with a macro that effectively
    changes the base layer, so that the appropriate nav layer can be
    accessed.

--- config/34keys.h
+++ config/34keys.h
@@ -1,9 +1,14 @@
 #define XXX &none
 #define ___ &trans
 
-#define BASE 0
-#define NAV 1
-#define NUM 2
+#define BASE_MAC 0
+#define BASE_LINUX 1
+#define NAV_MAC 2
+#define NAV_LINUX 3
+#define NUM 4
+
+#define BASE BASE_MAC BASE_LINUX
+#define NAV NAV_MAC NAV_LINUX
 
 // Apple's "Globe key" is mapped to Caps Lock at the operating system level
 // https://github.com/zmkfirmware/zmk/issues/947
