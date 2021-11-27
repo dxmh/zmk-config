61deab366e9b99e98b87ee9376509e4ab1885ffa
Dom H <dom@hxy.io>
2021-11-27 13:15:12 +0000

    Keep sticky keys active for 60s before timing out
    Because it's not currently possible to disable the timeout.

--- config/34keys.h
+++ config/34keys.h
@@ -16,3 +16,6 @@
 // Apple's "Globe key" is mapped to Caps Lock at the operating system level
 // https://github.com/zmkfirmware/zmk/issues/947
 #define GLOBE CAPSLOCK
+
+// Keep sticky keys active for a long time so they effectively do not time out
+#define STICKY_KEY_TIMEOUT 60000
