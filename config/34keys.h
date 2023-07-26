2dba23580c4ec437832c92f593330232e12b0c6a
Dom H <dom@hxy.io>
2023-07-26 11:46:07 +0100

    Remove `FUN` layer in favour of `MEH` key

--- config/34keys.h
+++ config/34keys.h
@@ -4,10 +4,13 @@
 #define BASE 0
 #define NAV 1
 #define NUM 2
-#define FUN 3
 
 #define SK(KEY) &nk KEY KEY
 #define MO(LAYER) &mo_tap LAYER F24
+
+// Use CONTROL+OPTION+COMMAND, so I still have a chance to use keybindings on a
+// default macOS keyboard (unlike hyper key which also includes SHIFT)
+#define MEH LC(LA(LGUI))
 
 // Keep sticky keys active for a long time so they effectively do not time out
 #define STICKY_KEY_TIMEOUT 60000
