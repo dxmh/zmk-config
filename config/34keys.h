0a37dc220c872c42465c1e14ae765cbfec08e28a
Dom H <dom@domhay.com>
2025-01-04 13:16:30 +0000

    Remove MEH key

--- config/34keys.h
+++ config/34keys.h
@@ -8,9 +8,5 @@
 #define SK(KEY) &nk KEY KEY
 #define MO(LAYER) &mo_tap LAYER F24
 
-// Use CONTROL+OPTION+COMMAND, so I still have a chance to use keybindings on a
-// default macOS keyboard (unlike hyper key which also includes SHIFT)
-#define MEH LC(LA(LGUI))
-
 // Keep sticky keys active for a long time so they effectively do not time out
 #define STICKY_KEY_TIMEOUT 60000
