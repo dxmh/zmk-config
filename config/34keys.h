6edee404197fb4b1e4f6a1d42692883a5e95e3c6
Dom H <dom@domhay.com>
2025-01-04 13:15:17 +0000

    Remove Globe key

--- config/34keys.h
+++ config/34keys.h
@@ -14,7 +14,3 @@
 
 // Keep sticky keys active for a long time so they effectively do not time out
 #define STICKY_KEY_TIMEOUT 60000
-
-// Apple "Globe" key
-// https://github.com/zmkfirmware/zmk/issues/947
-#define GLOBE CAPSLOCK
